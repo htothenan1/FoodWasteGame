f01d9535daff784aa436b201ab4f4ba6f7e9f966
Hernan <hberisso1@gmail.com>
2025-01-02 17:34:19 -0500

    yep

--- src/components/NewScene.js
+++ src/components/NewScene.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core"
 import { useLocation } from "react-router-dom"
-
 import { CSS } from "@dnd-kit/utilities"
 import { motion, AnimatePresence } from "framer-motion"
 import "../styles/NewScene.css"
