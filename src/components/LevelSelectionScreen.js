8be9f57702efcb5d0d555f1a77c79e989fa17b57
Hernan <hberisso1@gmail.com>
2025-01-15 13:31:30 -0500

    lock level progression

--- src/components/LevelSelectionScreen.js
+++ src/components/LevelSelectionScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import "../styles/LevelSelectionScreen.css"
-import { IconStar } from "@tabler/icons-react"
+import { IconStar, IconLock } from "@tabler/icons-react"
 import { levelData } from "../data/levelData"
 
 const LevelSelectionScreen = () => {
@@ -14,7 +14,13 @@ const LevelSelectionScreen = () => {
     setProgress(storedProgress)
   }, [])
 
+  const isLevelUnlocked = (level) => {
+    if (level === 1) return true // Level 1 is always unlocked
+    return progress[level - 1] >= 1 // Unlock if previous level has at least 1 star
+  }
+
   const handleLevelSelect = (id) => {
+    if (!isLevelUnlocked(id)) return
     const selectedLevel = levelData.find((level) => level.level === id)
     navigate(`/new-scene/`, {
       state: { items: selectedLevel.items, level: id },
@@ -30,27 +36,35 @@ const LevelSelectionScreen = () => {
       <div className="scene-container">
         <div className="backdrop"></div>
         <h1 className="level-title">Select a Level</h1>
-        {/* Quit Button - Fixed in Top Right Corner */}
+        <h1 className="sub-title">
+          Gain at least one star to unlock next level
+        </h1>
+
+        {/* Quit Button */}
         <button className="quit-button" onClick={handleRestart}>
           Quit Game
         </button>
+
+        {/* Level Grid */}
         <div className="level-grid">
           {levelData.map((level) => (
             <div
-              onClick={() => handleLevelSelect(level.level)}
               key={level.level}
-              className="level-card"
+              className={`level-card ${
+                isLevelUnlocked(level.level) ? "unlocked" : "locked"
+              }`}
+              onClick={() => handleLevelSelect(level.level)}
             >
               <h2>Level {level.level}</h2>
               <div className="stars-container">
                 {[...Array(3)].map((_, index) => (
                   <IconStar
+                    key={index}
                     fill={
                       index < (progress[level.level] || 0)
                         ? "#FFD700"
                         : "#D3D3D3"
                     }
-                    key={index}
                     color={
                       index < (progress[level.level] || 0)
                         ? "#FFD700"
@@ -59,6 +73,7 @@ const LevelSelectionScreen = () => {
                   />
                 ))}
               </div>
+              {!isLevelUnlocked(level.level) && <IconLock size={48} />}
             </div>
           ))}
         </div>
